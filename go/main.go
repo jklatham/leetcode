eb431afc3d3b7db922a4e5ba97b95b9585c5991a
Josh Latham <lathajos@oregonstate.edu>
2024-06-13 12:49:23 -0500

    2037 - Minimum Number of Moves to Seat Everyone

--- go/main.go
+++ go/main.go
@@ -6,12 +6,18 @@ import (
 )
 
 func main() {
-	arr1 := []int{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}
-	arr2 := []int{2, 1, 4, 3, 9, 6}
-	arr3 := []int{28, 6, 22, 8, 44, 17}
-	arr4 := []int{22, 28, 8, 6}
 
-	fmt.Println(problems.RelativeSortArray(arr1, arr2))
-	fmt.Println(problems.RelativeSortArray(arr3, arr4))
+	seats := []int{3, 1, 5}
+	students := []int{2, 7, 4}
+
+	seats2 := []int{2, 2, 6, 6}
+	students2 := []int{1, 3, 2, 6}
+
+	seats3 := []int{4, 1, 5, 9}
+	students3 := []int{1, 3, 2, 6}
+
+	fmt.Println(problems.MinMovesToSeat(seats, students))
+	fmt.Println(problems.MinMovesToSeat(seats2, students2))
+	fmt.Println(problems.MinMovesToSeat(seats3, students3))
 
 }
