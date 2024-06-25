f6176d81f3b77c6a959235dc73c7686bcacbd83b
Josh Latham <lathajos@oregonstate.edu>
2024-06-25 15:07:19 -0500

    1052 - Grumpy Bookstore Owner

--- go/problems/1052.go
+++ go/problems/1052.go
@@ -1,7 +1,5 @@
 package problems
 
-import "fmt"
-
 // since customers.length == grumpy.length - hash map?
 // if i >= len(grumpy)-minutes { grumpy[i] == 0 }
 
@@ -11,29 +9,35 @@ import "fmt"
 func MaxSatisfied(customers []int, grumpy []int, minutes int) int {
 	low := 0
 	high := 0
-	maxSat := 0
-	m1 := make(map[int][]int)
 
-	for i := 0; i < len(customers); i++ {
-		m1[i] = append(m1[i], customers[i])
-	}
-	fmt.Println(m1)
-
-	for high < (len(customers)) {
-		if (high-low+1) < minutes || grumpy[high] == 0 {
-			maxSat += customers[high]
-			//fmt.Println(customers[high], customers[low], maxSat)
-			high += 1
-
-		} else {
-			maxSat += customers[high]
-			maxSat -= customers[low]
-			//fmt.Println(customers[high], customers[low], maxSat)
-			high += 1
-			low += 1
-
+	// init
+	for i := range customers {
+		if grumpy[i] == 0 {
+			low += customers[i]
 		}
 	}
 
-	return maxSat
+	// additional satisfied customers if hidden tech used
+	for i := 0; i < minutes; i++ {
+		if grumpy[i] == 1 {
+			high += customers[i]
+		}
+	}
+
+	sat := high
+
+	// Sliding Window to find maximum additional customers
+	for i := minutes; i < len(customers); i++ {
+		if grumpy[i-minutes] == 1 {
+			high -= customers[i-minutes]
+		}
+		if grumpy[i] == 1 {
+			high += customers[i]
+		}
+		if high > sat {
+			sat = high
+		}
+	}
+
+	return low + sat
 }
