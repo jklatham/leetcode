edbe25208e94b9ebedd1d34eb80ce742c50b3450
jlatham <jlathamatx@gmail.com>
2024-06-28 14:37:36 -0500

    testing commit

--- go/main.go
+++ go/main.go
@@ -6,21 +6,7 @@ import (
 )
 
 func main() {
-
-	customers := []int{1, 0, 1, 2, 1, 1, 7, 5}
-	grumpy := []int{0, 1, 0, 1, 0, 1, 0, 1}
-	minutes := 3
-
-	c2 := []int{4, 10, 10}
-	g2 := []int{1, 1, 0}
-	m2 := 2
-
-	c3 := []int{5, 8}
-	g3 := []int{0, 1}
-	m3 := 1
-
-	fmt.Println("Total:", problems.MaxSatisfied(customers, grumpy, minutes))
-	fmt.Println("Total:", problems.MaxSatisfied(c2, g2, m2))
-	fmt.Println("Total:", problems.MaxSatisfied(c3, g3, m3))
-
+	numCities := 5
+	roads := [][]int{{0, 1}, {1, 2}, {2, 3}, {0, 2}, {1, 3}, {2, 4}}
+	fmt.Println(problems.MaximumImportance(numCities, roads))
 }
--- go/problems/2285.go
+++ go/problems/2285.go
@@ -1,8 +1,75 @@
 // 2285 - Maximum Total Importance of Roads
+/*
+You are given an integer n denoting the number of cities in a country.
+The cities are numbered from 0 to n - 1.
 
+You are also given a 2D integer array roads where roads[i] = [ai, bi]
+denotes that there exists a bidirectional road connecting cities ai and bi.
+
+You need to assign each city with an integer value from 1 to n, where each value can only be used once.
+The importance of a road is then defined as the sum of the values of the two cities it connects.
+
+Return the maximum total importance of all roads possible after assigning the values optimally.
+*/
+
+// map value 1..n to city
+// for each road return sum of value of connected cities
 package problems
 
+import (
+	"fmt"
+)
+
+/* // returns map of cities:value
+func getCityValueMap(n int) map[int]int {
+	cityValue := make(map[int]int)
+	for i := range n {
+		cityValue[i] = i
+	}
+	return cityValue
+} */
+
 func MaximumImportance(n int, roads [][]int) int64 {
-	var importance int64 = 43
-	return importance
+	cityValue := make(map[int]int)
+	//cityValue := make([]int, n)
+	fmt.Println(cityValue)
+	// Count connections in roads
+	for _, a := range roads {
+		for _, b := range a {
+			cityValue[b] += 1
+		}
+	}
+	fmt.Println(cityValue)
+
+	// Increment duplicates
+	/* for i := 0; i <= len(cityValue); i++ {
+		for j := 1; j < len(cityValue); j++ {
+			if i != j && cityValue[i] == cityValue[j] {
+				cityValue[j] += 1
+				j = 1
+			}
+		}
+	} */
+
+	i := 0
+
+	for i < len(cityValue) {
+		for next := range cityValue {
+			if cityValue[i] == cityValue[next] {
+				cityValue[next] += 1
+			}
+			i++
+		}
+	}
+	fmt.Println(cityValue)
+
+	// find importance
+	total := 0
+	for i := 0; i < len(roads); i++ {
+		total += (cityValue[roads[i][0]] + cityValue[roads[i][1]])
+	}
+	fmt.Println(total)
+	maximum := int64(total)
+
+	return maximum
 }
