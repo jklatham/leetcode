5871fda0d0b28b32621d89261410c54a34ce2b9f
Josh Latham <lathajos@oregonstate.edu>
2024-07-07 06:36:22 -0500

    1518 init

--- go/main.go
+++ go/main.go
@@ -7,17 +7,12 @@ import (
 
 func main() {
 
-	nums1 := []int{5, 3, 2, 4}
-	nums2 := []int{1, 5, 0, 10, 14}
-	nums3 := []int{3, 100, 20}
-	nums4 := []int{6, 6, 0, 1, 1, 4, 6} // expect 2 got 4
-	nums5 := []int{1, 5, 6, 14, 15}     // expect 1 got 4
-	nums6 := []int{82, 81, 95, 75, 20}  // expect 1 got 13
+	nb1 := 9
+	ne1 := 3
 
-	fmt.Println(problems.MinDifference(nums1))
-	fmt.Println(problems.MinDifference(nums2))
-	fmt.Println(problems.MinDifference(nums3))
-	fmt.Println(problems.MinDifference(nums4))
-	fmt.Println(problems.MinDifference(nums5))
-	fmt.Println(problems.MinDifference(nums6))
+	nb2 := 15
+	ne2 := 4
+
+	fmt.Println(problems.NumWaterBottles(nb1, ne1))
+	fmt.Println(problems.NumWaterBottles(nb2, ne2))
 }
---
+++ go/problems/1518.go
@@ -0,0 +1,6 @@
+// 1518 - Water Bottles
+package problems
+
+func NumWaterBottles(numBottles int, numExchange int) int {
+	return 1
+}
